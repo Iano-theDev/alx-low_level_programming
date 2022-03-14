d82624573404625dd3502f8927e68bcdb50847d2
Iano-theDev <sinceianmike@gmail.com>
2022-03-14 03:45:14 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main- entry point block
- * return- always o (success)
+ * Return- always o (success)
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
