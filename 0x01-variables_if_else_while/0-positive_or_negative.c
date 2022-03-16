9df0e8b724bd1317d57733cc6e9652811874b71c
Iano-theDev <sinceianmike@gmail.com>
2022-03-16 01:06:10 -0700

    use == instead of =

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if( n > 0 )
+	if(n > 0)
 		printf("is positive\n");
-	else if( n = 0)
+	else if(n == 0)
 		printf("is zero\n");
 	else
 		printf("is negatve\n");
