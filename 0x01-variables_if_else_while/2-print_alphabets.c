993c1a76170951c34b794de1cd196d48b76cd1fa
Iano-theDev <sinceianmike@gmail.com>
2022-03-16 09:34:20 -0700

    print lowecase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: program that prints the alphabet in lowercase, then uppercase.
+ * Return: 0;
+ */
+int main(void)
+{
+	char i;
+
+	for(i='a'; i<='z'; i++)
+        	putchar(i, ' ');
+		putchar('\n');
+	return (0);
+}
