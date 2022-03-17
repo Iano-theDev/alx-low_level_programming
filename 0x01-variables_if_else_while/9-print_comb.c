5d7c60d575ffe44b7b5226e9cd784adaf87841fa
Iano-theDev <sinceianmike@gmail.com>
2022-03-17 05:26:01 -0700

    All possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - using main toprint combination of numbers
+ * Description: print numbers seperated by comma and
+ * space in ascending order
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
