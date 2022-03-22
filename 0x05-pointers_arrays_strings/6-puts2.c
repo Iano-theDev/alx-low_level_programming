ba312b46c09d5dfdf3f6090bfe2457294672f173
Ian Kamande <60983828+Iano-theDev@users.noreply.github.com>
2022-03-22 09:18:03 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -23,17 +23,3 @@ void puts2(char *str)
 
 	putchar('\n');
 }
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *str;
-
-	str = "Holberton!\0Holberton";
-	puts2(str);
-	return (0);
-}
