8fb2893a78bef76ad051c5dae346b92984622f8f
Ian Kamande <60983828+Iano-theDev@users.noreply.github.com>
2022-05-26 21:25:56 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,44 @@
-#include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+
 /**
- * read_textfile - Reads a text file and prints it to the POSIX
- * standard output.
- * @filename: file.
- * @letters: Number of letters it should read and print.
- * Return: Actual number of letters it could read and print.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, res_read, res_write;
-	char *buf;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (0);
-	res_read = read(fd, buf, letters);
-	if (res_read == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		free(buf);
+		close(fd);
 		return (0);
 	}
-	res_write = write(STDOUT_FILENO, buf, res_read);
-	if (res_write == -1 || res_read != res_write)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
+	lenr = read(fd, buffer, letters);
 	close(fd);
-	return (res_write);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
