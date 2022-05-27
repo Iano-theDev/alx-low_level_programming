8537a6accd8df6c64a1dcc30227f30d176741329
Iano-theDev <sinceianmike@gmail.com>
2022-05-27 11:37:42 +0300

    Modified 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,28 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - reads a text file and prints it to the STDOUT
+ * @filename: is the name of the file
+ * @letters: number of letters it should read or print
  *
- * Return: actual number of letters it could read and print
+ * Return: Number of letters printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t r, w;
-	char *buffer;
+	ssize_t rd, op, wrt;
+	char *buff;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	op = open(filename, O_RDONLY);
+	if (op == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
 		return (0);
-	}
-	r = read(fd, buffer, letters);
-	close(fd);
-	if (r == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	w = write(STDOUT_FILENO, buffer, r);
-	free(buffer);
-	if (r != w)
-		return (0);
-	return (w);
+	rd = read(op, buff, letters);
+	wrt = write(STDOUT_FILENO, buff, rd);
+
+	close(op);
+	free(buff);
+	return (wrt);
 }
