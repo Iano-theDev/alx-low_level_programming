fde94b0b8c15e770d9a617dce354528fb28ce3d3
Iano-theDev <sinceianmike@gmail.com>
2022-05-27 12:14:51 +0300

    Re-formated main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,19 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
 //libraries
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <fcntl.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
-//Prototypes
-int _putchar(char c);
+
+//prototypes
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 
 
 #endif
