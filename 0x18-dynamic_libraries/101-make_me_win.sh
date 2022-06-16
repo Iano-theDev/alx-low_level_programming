4de80787719ee2f3d1c9d5dea96f9d4e84e5b3ee
IAN KAMANDE <60983828+Iano-theDev@users.noreply.github.com>
2022-06-16 13:31:25 +0300

    Make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Reggie071993/alx-low_level_programming/blob/main/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp/ https://raw.github.com/Iano-theDev/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
