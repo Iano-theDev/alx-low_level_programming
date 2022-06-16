ae2f52017326ded36d3e9d18ebb34c31c1966999
IAN KAMANDE <60983828+Iano-theDev@users.noreply.github.com>
2022-06-16 13:47:46 +0300

    remove prototype

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -11,6 +11,5 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-void set_string(char **s, char *to);
 
 #endif
