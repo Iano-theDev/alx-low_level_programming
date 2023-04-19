0264eae4ffd7711f3f42b70a71dc43de068602d5
Iano-theDev <sinceianmike@gmail.com>
2023-04-19 21:28:37 +0300

    add putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main -Entry point
+ *
+ * Description: prints "_putchar"
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
+
