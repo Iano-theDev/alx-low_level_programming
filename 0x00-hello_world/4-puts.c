e6367e88d1f6e874f5d22bc69047def510eaa616
Iano-theDev <sinceianmike@gmail.com>
2023-05-29 07:18:10 +0300

    Print string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, main block.
+ * Description: Uses puts to print a string in stdio
+ * Return: 0 always.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
