a9cf41933375a43d09ecdf4adaf275e7201751af
Iano-theDev <sinceianmike@gmail.com>
2023-05-29 23:17:59 +0300

    Test lat number of an output

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print the last digit of variable stored in @n
+ * @n: random integer
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	i = n % 10;
+	if (i > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	if (i == 0)
+		printf("Last digit of %d is %d and is 0\n", n, i);
+
+	if ((i < 6) && (i != 0))
+		printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, i);
+	return (0);
+}
