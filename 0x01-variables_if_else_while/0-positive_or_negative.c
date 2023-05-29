696b9b8085ee5ca32b3e04271c238f550e3bc236
Iano-theDev <sinceianmike@gmail.com>
2023-05-29 22:54:52 +0300

    '
    test sign of random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: assigns a random number to the variable n.
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ( n > 0 )
+		printf("is positive\n");
+	if ( n == 0 )
+		printf("is zero\n");
+	if ( n < 0  )
+		printf("is negative\n");
+	return (0);
+}
