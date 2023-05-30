1b324616f0e3e076415dc0852014fc5328085649
Iano-theDev <sinceianmike@gmail.com>
2023-05-30 13:49:56 +0300

    Update cleaner refactored code

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,35 +9,21 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	num1 = '0';
-	while (num1 < '9')
+	for (num1 = '0'; num1 <= '7'; num1++)
 	{
-		num2 = 1;
-		while (num2 < '9')
+		for (num2 = num1 + 1; num2 <= '8'; num2++)
 		{
-			if ((num1 != num2) && (num2 > num1))
+			for (num3 = num2 + 1; num3 <= '9'; num3++)
 			{
-				num3 = 2;
-				while (num3 <= '9')
-				{
-					if ((num2 != num3) && (num2 < num3))
-					{
-						putchar(num1);
-						putchar(num2);
-						putchar(num3);
-						if ((num1 == '7') && (num2 == '8') && (num3 == '9'))
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ');
-					}
-					num3++;
-				}
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+				if ((num1 == '7') && (num2 == '8') && (num3 == '9'))
+					break;
+				putchar(',');
+				putchar(' ');
 			}
-			num2++;
 		}
-		num1++;
 	}
 	putchar('\n');
 	return (0);
