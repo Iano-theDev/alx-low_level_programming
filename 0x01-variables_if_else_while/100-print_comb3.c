a682cea743d542ace3de6e7e7324c29230bb846d
Iano-theDev <sinceianmike@gmail.com>
2023-05-30 11:36:38 +0300

    Break at the end

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints all double digit combinations
+ * Return: 0 On Success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = '0';
+	while(i <= '9')
+	{
+		j = '1';
+		while(j <= '9')
+		{
+			if((i != j) && (i < j))
+			{
+				putchar(i);
+				putchar(j);
+				if(i == '8' && j == '9')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
