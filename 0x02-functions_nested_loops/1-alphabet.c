eb1966bcc155b417150a964e2fb747dec0f133a9
Iano-theDev <sinceianmike@gmail.com>
2023-05-31 00:10:00 +0300

    Test print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Description: Prints alph in lowercase using _putchar
+ * Return: Nothing, return is void
+ */
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	for (; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+}
