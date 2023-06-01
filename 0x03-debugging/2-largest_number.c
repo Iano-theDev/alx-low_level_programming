39d1f6e3895918c7aebbe5c9a435f4f32417d412
Iano-theDev <sinceianmike@gmail.com>
2023-06-02 00:35:18 +0300

    Add test cases for equal large numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,6 +24,12 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+	else if (a == b && a > c)
+		largest = a;
+	else if (a == c && a > b)
+		largest = a;
+	else if (b == c && b > a)
+		largest = b;
 
 	return (largest);
 }
