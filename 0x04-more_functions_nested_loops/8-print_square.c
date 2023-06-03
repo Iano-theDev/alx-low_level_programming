d7704aacb77a2b2da7ee81144a57d0fd919970fa
Iano-theDev <sinceianmike@gmail.com>
2023-06-03 14:28:42 +0300

    Fix: fix for cases 0 and below

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,14 +6,23 @@
  */
 void print_square(int size)
 {
-	int i, j;
 
-	for (i = 0; i < size; i++)
+
+	if (!(size <= 0))
 	{
-		for (j = 0; j < size; j++)
+		int i, j;
+
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
