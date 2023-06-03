4209e62bd7f1fb31855a6a0140257cde3b117b71
Iano-theDev <sinceianmike@gmail.com>
2023-06-03 12:33:46 +0300

    Print a diagonal line of backslashes

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line with a backslash
+ * @n: length of diagonal line
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
