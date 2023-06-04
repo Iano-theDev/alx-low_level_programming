cf55573db37ce55a99539daf506eafd2018df922
Iano-theDev <sinceianmike@gmail.com>
2023-06-05 00:41:21 +0300

    Incomplete triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the length of thhe triangle
+ * Return: nothing.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,5 +12,6 @@ void print_num(int n);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 
 #endif
