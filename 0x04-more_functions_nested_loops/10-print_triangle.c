4889ed8684e8f17d4b824118ea97b932699081b9
Iano-theDev <sinceianmike@gmail.com>
2023-06-11 12:48:50 +0300

    Update to print new line is size is less than or equal to 0

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,8 @@ void print_triangle(int size)
 {
 	int i, j;
 
+	if (size > 0)
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -20,4 +22,7 @@ void print_triangle(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
