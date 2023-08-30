ae4f11bf158660634060e95aca66588b5d723665
Iano-theDev <sinceianmike@gmail.com>
2023-08-30 16:28:29 +0300

    reverse a string via character swapping using duplicate string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char new_str[100];
+	char *ptr = s;
+	int n = 0;
+	
+	while (*s != '\0')
+	{
+		new_str[n] = *s;
+		n++;
+		s++;
+	}
+
+	n = 0;
+
+	while (s > ptr)
+	{
+		s--;
+		*s = new_str[n];
+		n++;
+	}
+}
