1a5145f1c211739361e2adcddd7c7d04fa2ed99c
Iano-theDev <sinceianmike@gmail.com>
2023-08-30 21:50:58 +0300

    test fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,6 +8,10 @@ int _putchar(int n);
 
 void _puts_recursion(char *s)
 {
+
+	if (*s == '\0')
+		_putchar('\n');
+
 	if (*s != '\0')
 	{
 		_putchar(*s);
@@ -16,6 +20,4 @@ void _puts_recursion(char *s)
 
 		_puts_recursion(s);
 	}
-
-	_putchar('\n');
 }
