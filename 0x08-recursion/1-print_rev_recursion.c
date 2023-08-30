b8b5a9e07907cb31dff4e68f5c6f9a68201d5258
Iano-theDev <sinceianmike@gmail.com>
2023-08-30 22:13:25 +0300

    reverse recursively

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _print_rev_recursion - prints string in reverse using recursion
+ * @s: pointer to string to be reversed
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	s--;
+	_putchar(*s);
+	_print_rev_recursive(s);
+}
