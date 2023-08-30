2a7f3090e5c6b063439b2c25610752b3c98af6fa
Iano-theDev <sinceianmike@gmail.com>
2023-08-30 21:36:10 +0300

    Puts recursive

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _puts_recursion - prints a string recursively followed by a new line
+ * @s: string pointer to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_putchar(*s);
+		_puts_recursion(*s);
+	}
+}
