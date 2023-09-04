65ceb3eb18b3381e6a26fda1186ae1caafe7fc4f
Iano-theDev <sinceianmike@gmail.com>
2023-09-04 06:37:43 +0300

    print executable name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - preints the name of the program
+ * @argc: arguments count to main
+ * @argv: argument vector array, all arguments of size argc
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
