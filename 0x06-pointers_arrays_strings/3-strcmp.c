9945b4291ba5478bf9613f16c38587517943ff01
Iano-theDev <sinceianmike@gmail.com>
2023-09-10 22:41:38 +0300

    Redo with a while loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0;
+	int i, res = 0;
 
-	while (*s1 != '\0')
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
+		if (s1[i] == s2[i])
 			continue;
-		}
 		else
 		{
-			result = s1 - s2;
+			res = s1[i] - s2[i];
 			break;
 		}
 	}
-	return (result);
+
+	return (res);
 }
