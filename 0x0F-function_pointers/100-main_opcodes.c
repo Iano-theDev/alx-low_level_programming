0691b7868a2a85ea4f6189ec41bfc08b9d037493
Iano-theDev <sinceianmike@gmail.com>
2023-09-14 06:07:09 +0300

    Fix betty checks

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -7,16 +7,17 @@
  *
  * Return: void
  */
- void print_opcodes(char *a, int n)
- {
-	  int i;
-	  for (i = 0; i < n; i++)
-        {
-                printf("%.2hhx", a[i]);
-                if (i < n - 1)
-                        printf(" ");
-        }
-        printf("\n");
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
 }
 /**
  * main - prints the opcodes of its own main function
@@ -27,18 +28,19 @@
  */
 int main(int argc, char **argv)
 {
-        int n;
-        if (argc != 2)
-        {
-                printf("Error\n");
-                exit(1);
-        }
-        n = atoi(argv[1]);
-        if (n < 0)
-        {
-                printf("Error\n");
-                exit(2);
-        }
-        print_opcodes((char *)&main, n);
-        return (0);
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
 }
