e1137c6f79263648510d75bddf6b7d9929f01c7c
Iano-theDev <sinceianmike@gmail.com>
2023-09-15 05:35:38 +0300

    More temporary fixes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,25 @@
 
 #include <stdio.h>
 
+/**
+ * struct fmt - Struct format
+ * @f: type format
+ * @p: print function
+*/
+typedef struct fmt
+{
+	char *f;
+	void (*p)(va_list s);
+} my_fmt;
+
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_c(va_list s);
+void print_i(va_list s);
+void print_f(va_list s);
+void print_s(va_list s);
 
 #endif /* #ifndef VAR_FUN_H */
