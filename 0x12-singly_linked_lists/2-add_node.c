92a72059dd2b14c94727bc4f78d61664b6a1972a
Iano-theDev <sinceianmike@gmail.com>
2023-09-25 15:11:44 +0300

    Add len function whithin the code

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,28 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+size_t len_s(char *str);
+
+/**
+ * len_s - gets the length of a string
+ * @str: a string
+ * Return: the length of str
+ */
+size_t len_s(char *str)
+{
+        size_t len = 0;
+        if (str)
+        {
+                while (*str != '\0')
+                {
+                        len++;
+                        str++;
+                }
+                return (len);
+        }
+        return (len);
+}
+
 /**
  * add_node - adds a node at the start of the linked list
  * @head: pointer to the first element in the list
