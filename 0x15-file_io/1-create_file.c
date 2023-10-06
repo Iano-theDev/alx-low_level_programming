70563656552f19b3bda434f873e2df66d0335a45
Iano-theDev <sinceianmike@gmail.com>
2023-10-06 18:46:14 +0300

    Fix: capture null text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 	while (text_content[len] != '\0')
 		len++;
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
