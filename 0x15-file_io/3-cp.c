2b028fe02de5f7c2df180f81203af0a9cbb1ee74
Iano-theDev <sinceianmike@gmail.com>
2023-10-07 05:50:34 +0300

    Update: program to copy contents of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,113 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * check_97 - checks argument count
+ * @argc: argument count
+ * Return: void
+ */
+void check_97(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+ * check_98 - checks if file exists
+ * @check: boolean
+ * @file: name of file
+ * @fd_from: file descriptor file_from
+ * @fd_to: file descriptor file_to
+ * Return: void
+ */
+void check_98(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
+	}
+}
+
+/**
+ * check_99 - checks if file exists
+ * @check: boolean
+ * @file: name of file
+ * @fd_from: file descriptor file_from
+ * @fd_to: file descriptor file_to
+ * Return: void
+ */
+void check_99(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
+	}
+}
+
+/**
+ * check_close - checks if file descriptors were closed
+ * @check: boolean
+ * @fd: file descriptor
+ * Return: void
+ */
+void check_close(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
+	char buffer[1024];
+	mode_t file_perm;
+
+	check_97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check_98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check_99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
+	{
+		lenr = read(fd_from, buffer, 1024);
+		check_98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check_99(lenw, argv[2], fd_from, fd_to);
+	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check_close(close_to, fd_to);
+	check_close(close_from, fd_from);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,5 +8,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void check_97(int argc);
+void check_98(ssize_t check, char *file, int fd_from, int fd_to);
+void check_99(ssize_t check, char *file, int fd_from, int fd_to);
+void check_close(int check, int fd);
 
 #endif /* MAIN_H */
