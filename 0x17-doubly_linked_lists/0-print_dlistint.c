4afa2b461e6b07acd3ccce5ce101d813fcc95e78
Iano-theDev <sinceianmike@gmail.com>
2024-04-27 08:38:49 +0300

    Removce desctiption

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,9 +4,8 @@
 /**
  * size_t print_dlistint - prints all the elemets of a dlistint_t list
  * @h: pointer to the head of a dlistint_t list
+ * 
  * Return: unsigned int, number of nodes
- *
- * Description: Prints all the elelments of a doubly linked list
  *
  */
 
