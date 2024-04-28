4c278796919071b7387fc67f4f82cede2a237191
Iano-theDev <sinceianmike@gmail.com>
2024-04-28 12:57:45 +0300

    Fix: typo on function name documentation

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_dlistint_t - frees  a dlistint_t list
+ * free_dlistint - frees  a dlistint_t list
  * @head: pointer to the start of the list
  *
  * Return: void
