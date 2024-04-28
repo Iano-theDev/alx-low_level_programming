56c5b11008029a182fb75cf4515aef987b84e216
Iano-theDev <sinceianmike@gmail.com>
2024-04-28 12:48:58 +0300

    Update: Add function to sum all data points of a linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data(n) of a linked list.
+ * @head: pointer to the start of the linked list
+ * 
+ * Return: sum of all data in a linked list, 0 otherwise
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int data_sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		data_sum += head->n;
+		head = head->next;
+	}
+	return (data_sum);
+}
