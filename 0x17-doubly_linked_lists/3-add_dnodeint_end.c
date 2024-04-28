dc13aae5cbfaf286bc79c8b381f7ebea66f4db76
Iano-theDev <sinceianmike@gmail.com>
2024-04-28 11:54:40 +0300

    Check for a null pointer to avoid derefrencing a null pointer

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,6 +12,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *temp, *new;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
